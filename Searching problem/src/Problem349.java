43e0aef41492356fd20b3dc61404152f086776ac
Yuanyuan <zky5@txstate.edu>
2024-03-29 16:40:44 -0500

    Using HashSet and HashMap to solve problem

--- Searching problem/src/Problem349.java
+++ Searching problem/src/Problem349.java
@@ -1,4 +1,7 @@
 // 349. Intersection of Two Arrays
+//Time complexoty of insert, find and delete methods of HashSet in Java is O(1)
+// Time complexity: O(n)
+
 
 import java.util.HashSet;
 import java.util.Set;
@@ -6,9 +9,11 @@ import java.util.Set;
 public class Problem349 {
     public int[] intersection(int[] nums1, int[] nums2){
         Set<Integer> set = new HashSet<>();
+//        O(n)
         for(int i = 0; i < nums1.length; i++){
             set.add(nums1[i]);
         }
+//        O(n)
         Set<Integer> resSet = new HashSet<>();
         for(int i = 0; i < nums2.length; i++){
             if(set.contains(nums2[i])){
--- Searching problem/src/Problem350.java
+++ Searching problem/src/Problem350.java
@@ -2,10 +2,13 @@ import java.util.Arrays;
 import java.util.HashMap;
 
 //Problem 350. Intersection of Two Arrays II
+//Time complexoty of insert, find and delete methods of HashMap in Java is O(1)
+// Time complexity: O(n)
+// Space complexity: O(n)
 public class Problem350 {
     public int[] intersect(int[] nums1, int[] nums2){
         HashMap<Integer,Integer> record = new HashMap<>();
-
+//O(n)
         for(int i: nums1){
             if(record.containsKey(i)){
                 record.put(i, record.get(i) + 1);
@@ -15,6 +18,7 @@ public class Problem350 {
         }
         int[] res = new int[0];
         int index = 0;
+//        O(n)
         for(int i : nums2){
             if(record.containsKey(i)){
                 res = Arrays.copyOf(res, res.length + 1);
