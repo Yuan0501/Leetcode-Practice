b694d94b3b13cf5754c7ca519742688c0853fc2c
Yuanyuan <zky5@txstate.edu>
2024-10-29 18:46:25 -0500

    dynamic programming

---
+++ Dynamic programming/src/Main.java
@@ -0,0 +1,8 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Problem70 slu = new Problem70();
+        System.out.println(slu.climbStairs(3));
+    }
+}
