1ecdb7c242aae410963692159bf797c8fe78ebf2
Yuanyuan <zky5@txstate.edu>
2025-01-11 21:39:41 -0600

    review

--- HashMap and HashSet/src/Codec.java
+++ HashMap and HashSet/src/Codec.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class Codec {
-}
+import java.util.HashMap;
+
+//535. Encode and Decode TinyURL
+public class Codec {
+
+    HashMap<Integer, String> map = new HashMap<>();
+    int count = 0;
+    // Encodes a URL to a shortened URL.
+    public String encode(String longUrl) {
+        map.put(count, longUrl);
+        return "http://tinyurl.com/" + count++;
+    }
+
+    // Decodes a shortened URL to its original URL.
+    public String decode(String shortUrl) {
+        String replace = shortUrl.replace("http://tinyurl.com/", "");
+        return map.get(Integer.parseInt(replace));
+    }
+
+    }
